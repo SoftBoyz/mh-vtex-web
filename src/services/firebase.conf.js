2ee8c51b7f2893a4cc865fe66988e064c577f1db
eric3119 <esc2@ic.ufal.br>
2020-04-29 22:14:44 -0300

    first commit

---
+++ src/services/firebase.conf.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyDKJ0dmRDxXVc-zUHd8XPHZ-zbFMK0dSk8",
+    authDomain: "mega-hack-vtex.firebaseapp.com",
+    databaseURL: "https://mega-hack-vtex.firebaseio.com",
+    projectId: "mega-hack-vtex",
+    storageBucket: "mega-hack-vtex.appspot.com",
+    messagingSenderId: "578307744702",
+    appId: "1:578307744702:web:aa97656a5f97c986554fef"
+};
+// Initialize Firebase
+const firebaseApi = firebase.initializeApp(firebaseConfig);
+export default firebaseApi;
