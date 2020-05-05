d8d008d61547c141317e169327a5a693754edc00
eric3119 <esc2@ic.ufal.br>
2020-05-04 22:01:58 -0300

    add money mask && change header color

--- src/assets/jss/material-dashboard-react.js
+++ src/assets/jss/material-dashboard-react.js
@@ -70,7 +70,7 @@ const defaultFont = {
   lineHeight: "1.5em"
 };
 
-const primaryColor = ["#3f51b5", "#0336ff", "#6b38fb", "#6200ee"];
+const primaryColor = ["#3f51b5", "#00c1d9", "#0094a6", "#6200ee"];
 const warningColor = ["#ff9800", "#ffa726", "#fb8c00", "#ffa21a"];
 const dangerColor = ["#f44336", "#ef5350", "#e53935", "#f55a4e"];
 const successColor = ["#4caf50", "#66bb6a", "#43a047", "#5cb860"];
